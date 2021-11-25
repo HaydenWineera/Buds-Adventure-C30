40054614e226b12f499f5aff9a3144d39659ae8e
HaydenWineera <haydenwin04@gmail.com>
2021-11-25 11:20:20 -0600

    changed stings to make sense

--- sketch.js
+++ sketch.js
@@ -13,15 +13,15 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   state = "menuScreen";
-  let txt = createDiv("This is an HTML string!");
-  txt.position(600, 400);
+  let txt = createDiv("Press Me!");
+  txt.position(450, 700);
 }
 
 function draw() {
   if (state === "menuScreen") {
     background(25);
     textSize(32);
-    text("Lights Out", windowWidth/2, windowHeight/2);
+    text("Menu Screen", windowWidth/2, windowHeight/2);
     fill(0, 102, 153);
   }
   if (state === "intro") {
