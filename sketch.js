26ac8cc4496f201f09b594b1f1a2f21e5d356dd9
HaydenWineera <haydenwin04@gmail.com>
2021-11-29 12:11:30 -0600

    tring to figure out css and html

--- sketch.js
+++ sketch.js
@@ -9,9 +9,9 @@ let someText;
 
 function setup() {
   let canvas = createCanvas(1760, 600);
-  canvas.position(10, 30);
+  canvas.position(10, 10);
 
-  someText = createP("Some HTML string");
+  someText = createP("The story begins <span style=\"color: white;\">here</span>");
   someText.position(500, 500);
   someText.style("color", "red");
   someText.style("font-size", "42pt");
@@ -19,9 +19,9 @@ function setup() {
 
 function draw() {
   background(0);
-  circle(mouseX, mouseY, 100);
 }
 
 function mousePressed() {
-  someText.html("What <em>you<em> change me"); 
+  someText.html = "keep <span style=\"color: white;\">clicking!</span>"; 
+  // supposed to look like <span style="color: white;">WORD</span>
 }
