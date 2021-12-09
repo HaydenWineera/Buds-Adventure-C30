183c753b9bcdfe851b3121656e78cf6821f1f2bf
HaydenWineera <haydenwin04@gmail.com>
2021-12-09 10:20:17 -0600

    trying to figure out this library

--- sketch.js
+++ sketch.js
@@ -18,11 +18,7 @@ function setup() {
   someText.style("color", "red");
   someText.style("font-size", "42pt");
 
-  instance = new TypeIt("#type-effect", {
-    strings: "Welcome, user, to the best text-based game you've ever played! Whenever your ready just click your LEFT MOUSE BUTTON and we will start your story...",
-    speed: 2,
-  });
-  instance.go();
+  typeWrite("hey there this is just going to type something hopefully");
 }
 
 function draw() {
@@ -30,16 +26,10 @@ function draw() {
 }
 
 function mousePressed() {
-  instance.reset();
-  instance.delete();
-  instance = new TypeIt("#type-effect", {
-    speed: 70,
-  })
-    .type("You are a detective, Marcus Maclain. And you are currently driving to a crime scene.")
-    .pause(500)
-    .type(" All you know is that you recieved a very urgent call from your collegues.")
-    .pause(500)
-    .type("\n\"What is going on?\" You think to yourself\n")
-    .go();
+
   //when pressed it should change the words that show up...
+}
+
+function typeWrite(sentence) {
+  $("#typeArea").clearIt().typeIt(sentence, 0.05, "text").hideCursor();
 }
