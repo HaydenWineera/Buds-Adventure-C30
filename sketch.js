61661ac105b9d61f0b1434c5c8b8938a1489e1c5
HaydenWineera <haydenwin04@gmail.com>
2021-12-15 11:54:32 -0600

    making a menu screen

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ let counter = 0;
 let speed = 10;
 
 // story globals
-let introSeq = ['It is a cloudy and very foggy day in Shaughnessy Vancouver. In your patrol vehicle you can barely see 10 feet in front of you...','As you struggle to keep an eye on the road ahead, you barely notice the red light eminating from within the fog. You feel a pump of adrenaline and a shiver down your spine as you screech to a sudden stop...', '"God its a miserable day out today", you think to yourself... ', 'Life in Shaughnessy is pretty boring, but your job as a detective pays well so you choose not to complain...', 'The light turns green, as it does so you here another car engine rapidly approaching the intersection...', 'you see a faint black outline of what looks like an SUV before it disapears into the mist, it definately was not going the speed limit...', 'Even though you should probab;y follow up on this, you decide not too because your too lazy and-\n"What time is it?", you think to yourself...', 'You look down at your watch, the sharp LEDs of your watch display 5:30...', '"Im off duty", you think to yourslef,"I cant go after them now, maybe later", as you mumble you let out an exhausting exhale...', 'It is true, right now you hold the department record for most arrests, as you think this you smirk']; // introduction \033[3mI\033[0m
+let introSeq = ['It is a cloudy and very foggy day in Shaughnessy Vancouver. In your patrol vehicle you can barely see 10 feet in front of you...','As you struggle to keep an eye on the road ahead, you barely notice the red light eminating from within the fog. You feel a pump of adrenaline and a shiver down your spine as you screech to a sudden stop...', '"God its a miserable day out today", you think to yourself... ', 'Life in Shaughnessy is pretty boring, but your job as a detective pays well so you choose not to complain...', 'The light turns green, as it does so you here another car engine rapidly approaching the intersection...', 'you see a faint black outline of what looks like an SUV before it disapears into the mist, it definately was not going the speed limit...', 'Even though you should probab;y follow up on this, you decide not too because your too lazy. "What time is it?", you think to yourself...', 'You look down at your watch, the sharp LEDs of your watch display 5:30...', '"Im off duty", you think to yourslef,"I cant go after them now, maybe later", as you mumble you let out an exhausting exhale...', 'It is true, right now you hold the department record for most arrests, as you think this you smirk']; // introduction \033[3mI\033[0m
 
 function setup() {
   let canvas = createCanvas(1760, 760);
@@ -39,9 +39,8 @@ function mousePressed() {
 }
 
 function keyPressed(sentence) {
-  if (value === 32) {
-    typeWrite(sentence, 0.01, "text") = 10;
-    
+  if (value = 80) {
+    $("typeArea");  
   }
 }
 function typeWrite(sentence) {
