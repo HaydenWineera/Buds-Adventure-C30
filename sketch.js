bf53623d52ef4a6e55473c09db08384c25aef48d
HaydenWineera <91340486+HaydenWineera@users.noreply.github.com>
2022-01-10 13:42:53 -0600

    Addded a state variable and added an image to go with the text

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // Light's Out
 // Hayden Wineera
-// 11/17/2021
+// 2022/01/10
 
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -10,10 +10,17 @@ let someText;
 let instance;
 let counter = 0;
 let speed = 0.03;
+let introSeqPic;
+let picSize;
+let state = 0;
 
 // story globals
 let introSeq = ["If at any point you wish to quit, just leave the browser. BE WARNED PROGRESS WILL NOT BE SAVED...", "Let us begin, shall we?...", "You buckle up the seatbelt of your squad car. Check the side mirrors, the rear view. All looks well...", "You grab the keys out of your pocket and put them into the ignition. The engine starts without a stutter. And you are on your way...", "The streets are littered with the homeless. You can't help but be sorry for some of them. They all start to hurry away at the sight of your car, no one likes the police around here...", "An ominous fog starts rolling in. You've noticed it happens a lot more now. ''Probably climate change'', you think to yourself...","This fog does start to worry you though, the roads start to become more troublesome to drive through. Even though you know them like the back of your hand, you still have to worry about other cars.",'As you struggle to keep an eye on the road ahead, you barely notice the red light eminating from within the fog. You feel a pump of adrenaline and a shiver down your spine as you screech to a sudden stop...', "''God what's happening today?'', you think to yourself... ", "Still stopped you here a distant noise, another car approaching the intersection...", 'The light turns green, and as it does so the car engine starts roaring louder within the fog...', 'The car drifts into the intersection before your eyes. You see a faint black outline of what looks like an SUV before it disapears into the mist, it definately was not going the speed limit...', "''Christ! I should go after them!'' You step on the gas pedal, adrenaline starts pumping through your veins. This is why you became a cop..."]; // introduction \033[3mI\033[0m
 let chaseSeq = ["howdy ho"];
+
+function preload() {
+  introSeqPic = loadImage('assets/Scene 1.jpg');
+}
 
 function setup() {
   let canvas = createCanvas(1760, 760);
@@ -29,10 +36,18 @@ function setup() {
 
 function draw() {
   background(0);
+  if (state = 0) {
+    background(0);
+  }
+  if (state = 1) {
+    backround(0);
+    image(introSeqPic, 0, 0, 1760, 760);
+  }
 }
 
 function mousePressed() {
   //when the left mouse is clicked it should change the words that show up
+    state = 1;
     $("#typeArea").clearIt();
     typeWrite(introSeq[counter]);
     $("typeArea").typeIt;
